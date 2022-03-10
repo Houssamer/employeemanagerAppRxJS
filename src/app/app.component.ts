f2d3ef03d3ba42628ae3fb539eb3795c2711c4c4
Houssamer <riad.houssame.eleve@ehtp.ac.ma>
2022-03-10 11:31:14 +0000

    Service Implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { EmployeeService } from './services/employee.service';
+import { Employee } from './Employee';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'employeemanagerAppRxJS';
+  private employeesSubject = new BehaviorSubject<Employee[]>([]);
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+
+  }
+
 }
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,52 @@
+import { Employee } from './../Employee';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private apiUrl: string = 'http://localhost:8080/employee';
+
+  constructor(private http: HttpClient) {}
+
+  $getAllEmployee = <Observable<Employee[]>>(
+    this.http
+      .get<Employee[]>(`${this.apiUrl}/all`)
+      .pipe(tap(console.log), catchError(this.handleError))
+  );
+
+  $getEmployeeById = (id: number) =>
+    <Observable<Employee>>(
+      this.http
+        .get<Employee>(`${this.apiUrl}/find/${id}`)
+        .pipe(tap(console.log), catchError(this.handleError))
+    );
+
+  $addEmployee = (employee: Employee) =>
+    <Observable<Employee>>(
+      this.http
+        .post<Employee>(`${this.apiUrl}/add`, employee)
+        .pipe(tap(console.log), catchError(this.handleError))
+    );
+
+  $updateEmployee = (employee: Employee) =>
+    <Observable<Employee>>(
+      this.http
+        .post<Employee>(`${this.apiUrl}/update`, employee)
+        .pipe(tap(console.log), catchError(this.handleError))
+    );
+
+  $deleteEmployee = (id: number) =>
+    <Observable<any>>(
+      this.http
+        .delete(`${this.apiUrl}/delete/${id}`)
+        .pipe(tap(console.log), catchError(this.handleError))
+    );
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error);
+    return throwError(() => new Error('An error has been occured'));
+  }
+}
